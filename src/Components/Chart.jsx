147c5f9977257898bbe65f27c85e343d4f94a920
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-07-18 12:39:38 +0200

    Add files via upload

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { VictoryLine, VictoryChart, VictoryTheme } from "victory"
+import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis } from "victory"
 
 const Chart = ({ data }) => {
   return (
     <div className="chart">
       <VictoryChart theme={VictoryTheme.material}>
         <VictoryLine
-          width={100}
+          width={200}
           style={{
             data: { stroke: "#c43a31" },
             parent: { border: "1px solid #ccc" },
@@ -18,6 +18,12 @@ const Chart = ({ data }) => {
           data={data}
           y="weight"
           x="date"
+        />
+        <VictoryAxis dependentAxis />
+        <VictoryAxis
+          style={{
+            tickLabels: { angle: 45, transform: "translate(20, 12)" },
+          }}
         />
       </VictoryChart>
     </div>
